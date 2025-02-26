83c3902799fc4cdd1350643827d9733e226de2a8
AntobioNallez <89712714+AntobioNallez@users.noreply.github.com>
2025-02-26 10:12:06 +0100

    Doble Salto
    Se ha añadido el doble salto y he comenzado con el salto de pared, también he añadido un modificador para la gravedad

--- Assets/Scripts/JugadorScript.cs
+++ Assets/Scripts/JugadorScript.cs
@@ -6,23 +6,39 @@ using UnityEngine.InputSystem;
 public class JugadorScript : MonoBehaviour
 {
     public Rigidbody2D rb;
+
+    [Header("Movimiento")]
     public float moveSpeed = 5f;
     float horizontalMovement;
-    public float jumpPower = 3f;
 
+    [Header("Doble Salto")]
+    public float jumpPower = 3f;
+    public int saltoMax = 2;
+    int saltos;
+
+    [Header("Ground Check")]
     public Transform groundCheckPos;
     public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
     public LayerMask groundLayer;
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
+    //TODO FINALIZAR EL SALTO DE PARED
+    [Header("Wall Check")]
+    public Transform wallCheckPos;
+    public Vector2 wallCheckSize = new Vector2(0.49f, 0.03f);
+    public LayerMask wallLayer;
+    
+
+    [Header("Gravedad")]
+    public float gravedadBase = 2f;
+    public float speedCaidaMax = 15f;
+    public float multiplicadorSpeedCaida = 1.3f;
 
     // Update is called once per frame
     void Update()
     {
         rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
+        GroundCheck();
+        Gravedad();
     }
 
     public void Mover(InputAction.CallbackContext context)
@@ -30,21 +46,50 @@ public class JugadorScript : MonoBehaviour
         horizontalMovement = context.ReadValue<Vector2>().x;
     }
 
-    public void salto(InputAction.CallbackContext context)
+    public void Salto(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (saltos > 0)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            if (context.performed)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+                saltos--;
+            }
+            else if (context.canceled)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0);
+                saltos--;
+            }
         }
-        else if (context.canceled)
+    }
+
+    //NO FUNCIONA CORRECTAMENTE REVISAR
+    private void Gravedad()
+    {
+        if (rb.velocity.y < 0)
         {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0);
+            rb.gravityScale = gravedadBase * multiplicadorSpeedCaida;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -speedCaidaMax));
+        }
+        else
+        {
+            rb.gravityScale = gravedadBase;
+        }
+    }
+
+    private void GroundCheck()
+    {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {
+            saltos = saltoMax;
         }
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.white;
-        Gizmos.DrawCube(groundCheckPos.position, groundCheckSize);
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(wallCheckPos.position, wallCheckSize);
     }
 }
