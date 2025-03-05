75a036b8d5c579d60ef8cb31cf966df8527d9b24
AntobioNallez <89712714+AntobioNallez@users.noreply.github.com>
2025-03-05 14:13:27 +0100

    Update 5 marzo
    Slide y flip

--- Assets/Scripts/JugadorScript.cs
+++ Assets/Scripts/JugadorScript.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class JugadorScript : MonoBehaviour
 {
     public Rigidbody2D rb;
-    bool isFacingRight = false;
+    bool isFacingRight = true;
 
     [Header("Movimiento")]
     public float moveSpeed = 5f;
@@ -19,20 +19,31 @@ public class JugadorScript : MonoBehaviour
 
     [Header("Ground Check")]
     public Transform groundCheckPos;
-    public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.04f);
     public LayerMask groundLayer;
+    bool isGrounded;
 
     //TODO FINALIZAR EL SALTO DE PARED
     [Header("Wall Check")]
     public Transform wallCheckPos;
-    public Vector2 wallCheckSize = new Vector2(0.49f, 0.03f);
+    public Vector2 wallCheckSize = new Vector2(0.05f, 0.5f);
     public LayerMask wallLayer;
-    
 
     [Header("Gravedad")]
     public float gravedadBase = 2f;
     public float speedCaidaMax = 15f;
     public float multiplicadorSpeedCaida = 1.3f;
+
+    [Header("Movimiento Pared")]
+    public float velocidadPared = 2f;
+    bool deslizamientoPared;
+
+    //Salto de pared
+    bool isWallJumping;
+    float wallJumpDirection;
+    float wallJumpTime = 0.5f;
+    float wallJumpTimer;
+    Vector2 wallJumpPower = new Vector2(5f, 10f);
 
     // Update is called once per frame
     void Update()
@@ -40,6 +51,7 @@ public class JugadorScript : MonoBehaviour
         rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
         GroundCheck();
         Gravedad();
+        DeslizamientoPared();
         Flip();
     }
 
@@ -63,9 +75,15 @@ public class JugadorScript : MonoBehaviour
                 saltos--;
             }
         }
+
+        //salto de pared
+        if(context.performed && wallJumpTimer > 0f) {
+            isWallJumping = true;
+            rb.velocity = new Vector2(wallJumpDirection * wallJumpPower.x, wallJumpPower.y);
+        } 
     }
 
-    //NO FUNCIONA CORRECTAMENTE REVISAR
+    //NO FUNCIONA CORRECTAMENTE REVISAR (CREO QUE FUNCIONA HE DE REVISAR)
     private void Gravedad()
     {
         if (rb.velocity.y < 0)
@@ -79,11 +97,34 @@ public class JugadorScript : MonoBehaviour
         }
     }
 
-    private void Flip() {
-        if(isFacingRight && horizontalMovement < 0 || !isFacingRight && horizontalMovement > 0) {
+    private void DeslizamientoPared()
+    {
+        //No tiene que estar tocando el suelo y tiene que estar en una pared
+        if (!isGrounded && WallCheck() && horizontalMovement != 0)
+        {
+            deslizamientoPared = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -velocidadPared));
+        }
+        else
+        {
+            deslizamientoPared = false;
+        }
+    }
+
+    private void ProcessWallJump() {
+        if(deslizamientoPared) {
+            deslizamientoPared = false;
+        }
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontalMovement < 0 || !isFacingRight && horizontalMovement > 0)
+        {
             isFacingRight = !isFacingRight;
             Vector3 ls = transform.localScale;
-            ls.x = -1f;
+            ls.x *= -1f;
+            transform.localScale = ls;
         }
     }
 
@@ -92,7 +133,17 @@ public class JugadorScript : MonoBehaviour
         if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
         {
             saltos = saltoMax;
+            isGrounded = true;
         }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    private bool WallCheck()
+    {
+        return Physics2D.OverlapBox(wallCheckPos.position, wallCheckSize, 0, wallLayer);
     }
 
     private void OnDrawGizmosSelected()
